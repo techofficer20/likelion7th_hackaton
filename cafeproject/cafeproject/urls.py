4026e70fd5b6613580699a5c32d74139b5f0b790
cheche1210 <invictuscheche@gmail.com>
2019-08-03 16:34:18 +0900

    haeun commit

--- cafeproject/cafeproject/urls.py
+++ cafeproject/cafeproject/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import mainpage.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('searchapp.urls')),
-]
+    path('',mainpage.views.main, name="main"),
+
+    path('searchapp', include('searchapp.urls')),
+    
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
