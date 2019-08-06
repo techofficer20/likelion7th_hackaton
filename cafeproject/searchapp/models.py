54aaa82a107a93ba69528de76e81577543887a30
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-08-07 02:58:15 +0900

    search commit

--- cafeproject/searchapp/models.py
+++ cafeproject/searchapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length = 20)
+    body = models.TextField(max_length = 100)
+
+    def __str__(self):
+        return self.title
--- cafeproject/searchapp/views.py
+++ cafeproject/searchapp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
-
+from .models import Post
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    post = Post.objects.all()
+    return render(request, 'index.html', {'post' : post})
 
 def result(request):
+    post = Post.objects.all()
     obj = request.GET['searching'] # 검색어 데이터 얻어냄
-    return render(request, 'result.html', {'obj' : obj}) # 검색어 데이터를 html에 표현하기 위해 딕셔너리 추가
+    return render(request, 'result.html', {'post' : post, 'obj' : obj}) # 검색어 데이터를 html에 표현하기 위해 딕셔너리 추가
