90bb7bd1f3bb5875e16954c2ba046671d91d5e8d
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-08-07 16:16:25 +0900

    search update commit

--- cafeproject/searchapp/models.py
+++ cafeproject/searchapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length = 20)
-    body = models.TextField(max_length = 100)
-
+    tag_body = models.TextField(max_length = 100)
+    location = models.CharField(max_length = 15)
     def __str__(self):
         return self.title
