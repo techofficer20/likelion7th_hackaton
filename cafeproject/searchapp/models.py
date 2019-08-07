cf3522e394416a65bc383159dd950eba59347cd5
jungss98 <sjj98@naver.com>
2019-08-07 17:28:05 +0900

    starate

--- cafeproject/searchapp/models.py
+++ cafeproject/searchapp/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 # Create your models here.
+one_to_five_chocies = zip(range(1,5+1), range(1,5+1))
 
 class Post(models.Model):
     title = models.CharField(max_length = 20)
     tag_body = models.TextField(max_length = 100)
     location = models.CharField(max_length = 15)
+    starrate = models.SmallIntegerField(choices=one_to_five_chocies, default=3)
     def __str__(self):
         return self.title
